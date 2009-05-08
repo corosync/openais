c8d1a5f86fa4df013cd6ebe52bcf0be238d18dd7
Steven Dake <sdake@redhat.com>
2009-05-08 22:44:02 +0000

    Fix ppc64 ipc regression.
    git-svn-id: https://svn.fedorahosted.org/svn/openais/branches/whitetank@1890 fd59a12c-fef9-0310-b244-a6a79926bd2f

--- lib/util.c
+++ lib/util.c
@@ -264,6 +264,16 @@ priv_change_send (struct ipc_segment *ipc_segment)
 	return (0);
 }
 	
+	
+#if defined(_SEM_SEMUN_UNDEFINED)
+union semun {
+	int val;
+	struct semid_ds *buf;
+	unsigned short int *array;
+	struct seminfo *__buf;
+};
+#endif
+
 SaAisErrorT
 openais_service_connect (
 	enum service_types service,
@@ -278,6 +288,7 @@ openais_service_connect (
 	int res;
 	mar_req_setup_t req_setup;
 	mar_res_setup_t res_setup;
+	union semun semun;
 
 	res_setup.error = SA_AIS_ERR_LIBRARY;
 
@@ -336,12 +347,18 @@ openais_service_connect (
 		goto error_exit;
 	}
 	
-	res = semctl (ipc_segment->semid, 0, SETVAL, 0);
+	semun.val = 0;
+	res = semctl (ipc_segment->semid, 0, SETVAL, semun);
 	if (res != 0) {
 		goto error_exit;
 	}
 
-	res = semctl (ipc_segment->semid, 1, SETVAL, 0);
+	res = semctl (ipc_segment->semid, 1, SETVAL, semun);
+	if (res != 0) {
+		goto error_exit;
+	}
+
+	res = semctl (ipc_segment->semid, 2, SETVAL, semun);
 	if (res != 0) {
 		goto error_exit;
 	}
